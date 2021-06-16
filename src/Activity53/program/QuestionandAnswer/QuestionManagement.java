0c521fb63f52635fdc4a7e6cbdaed98f52c87cfb
diemkhang1912@gmail.com <diemkhang1912@gmail.com>
2021-06-16 12:26:06 +0700

    Q&A

--- src/Activity53/program/QuestionandAnswer/QuestionManagement.java
+++ src/Activity53/program/QuestionandAnswer/QuestionManagement.java
@@ -15,10 +15,20 @@ public class QuestionManagement {
     }
     // Kiểm tra độ khó
     public int find(int dokho){
-        for(int i=0;i<questions.length;i++){
-            if(dokho==questions[i].getLevel()){
-                System.out.println(" - Question"+(i+1)+": "+questions[i].getContent());
-            }
+        System.out.println("Độ khó "+dokho+" có các câu hỏi: ");
+        int j=0;
+        for(int i=0;i<questions.length;i++) {
+            if (dokho == questions[i].getLevel()) {
+                System.out.println(" - Question" + (i + 1) + ": " + questions[i].getContent());
+                System.out.println("            A. " + questions[i].getAnswer1());
+                System.out.println("            B. " + questions[i].getAnswer2());
+                System.out.println("            C. " + questions[i].getAnswer3());
+                System.out.println("            D. " + questions[i].getAnswer4());
+            }else j++;
+        }
+        if (j== questions.length){
+            System.out.println("+ Rất tiếc. Độ khó bạn chọn không có trong câu hỏi nào!");
+            questions = new Question[0];
         }
         return dokho;
     }
--- src/Activity53/program/QuestionandAnswer/Test.java
+++ src/Activity53/program/QuestionandAnswer/Test.java
@@ -19,12 +19,25 @@ public class Test {
             System.out.print("  + Do kho (1-3): ");
             int level = input.nextInt();
             input.nextLine();
+
+            System.out.print("  + Nhap vao Answer A: ");
+            String A = input.nextLine();
+            System.out.print("  + Nhap vao Answer B: ");
+            String B = input.nextLine();
+            System.out.print("  + Nhap vao Answer C: ");
+            String C = input.nextLine();
+            System.out.print("  + Nhap vao Answer D: ");
+            String D = input.nextLine();
             System.out.print("  + Nhap vao dap an dung (A,B,C,D): ");
             String correct = input.nextLine();
             questions[i] = new Question();
             questions[i].setContent(content);
             questions[i].setLevel(level);
             questions[i].setCorrect(correct);
+            questions[i].setAnswer1(A);
+            questions[i].setAnswer2(B);
+            questions[i].setAnswer3(C);
+            questions[i].setAnswer4(D);
         }
         System.out.println();
         QuestionManagement questionManagement = new QuestionManagement(questions);
@@ -33,35 +46,32 @@ public class Test {
         int dokho = input.nextInt();
         input.nextLine();
         // Hien thi ra man hinh nhung cau hoi tuong ung voi do kho
-        System.out.println("Do kho "+dokho+" co nhung cau hoi sau: ");
         questionManagement.find(dokho);
         System.out.println();
         // Nhập vào đáp án theo đúng thứ tự từ câu hỏi đầu tiên đến cuối cùng
-        System.out.print("Nhap dap an cua nhung cau hoi do theo thu tu: ");
+        System.out.print("Nhap dap an theo thu tu 1,2,3,4,...: ");
         String dapan = input.nextLine(); // muốn trả lời cau hoi 1,3,5. Nhập: A,0,A,0,C
-        int j=0,k=0;
+        int j = 0, k = 0;
         // Hiển thị ra số câu trả lời đúng và đáp án đúng của từng câu hỏi.
-        for(int i=0;i<questions.length;i++){
-            if (dokho==questions[i].getLevel()){
-                j+=1;
-                System.out.println("- Question "+(i+1)+":");
-                char c = dapan.charAt(i*2);
-                System.out.println("Your Answer: "+c);
+        for (int i = 0; i < questions.length; i++) {
+            if (dokho == questions[i].getLevel()) {
+                j += 1;
+                System.out.println("- Question " + (i + 1) + ":");
+                char c = dapan.charAt(i * 2);
+                System.out.println("     Your Answer: " + c);
                 char out = questions[i].getCorrect().charAt(0);
-                if(c == out){
+                if (c == out) {
                     questions[i].answerCorrect(true);
                     k++;
-                }else questions[i].answerCorrect(false);
-               System.out.println("Answer correct: "+questions[i].getCorrect());
+                } else questions[i].answerCorrect(false);
+                System.out.println("     Answer correct: " + questions[i].getCorrect());
             }
         }
         System.out.println();
-        System.out.println(questionManagement.score(k,j));
-    }
+        System.out.println("" + questionManagement.score(k, j));
 
+    }
     public static void main(String[] args) {
         Test test = new Test();
     }
-
-
 }
